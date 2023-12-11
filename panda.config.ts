a07b7c5b07ba97e55d0e99149d693ca103f6ccc7
undefined <johann@objekt.stream>
2023-12-11 01:54:23 +0100

    ~

--- panda.config.ts
+++ panda.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "@pandacss/dev";
 
 export default defineConfig({
   // Whether to use css reset
-  preflight: false,
+  preflight: true,
 
   // Where to look for your css declarations
   include: ['./src/**/*.{ts,tsx,js,jsx,astro}', './pages/**/*.{ts,tsx,js,jsx,astro}'],
@@ -16,6 +16,14 @@ export default defineConfig({
   },
 
   prefix: "panda",
+
+  layers: {
+    utilities: "panda_utilities",
+    reset: "panda_reset",
+    base: "panda_base",
+    tokens: "panda_tokens",
+    recipes: "panda_recipes",
+  },
 
   // The output directory for your css system
   outdir: "styled-system",
